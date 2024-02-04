a02bfac476860f46156a61b82fafcb595a9b0a14
rodrigo0345 <rodrigocralha@gmail.com>
2024-02-04 18:31:14 +0000

    adding vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === "production" ? "/portfolio-2.0/assets" : "/",
+    publicPath: process.env.NODE_ENV === "production" ? "/portfolio-2.0" : "/",
 };
